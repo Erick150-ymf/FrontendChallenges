eb058464f1fa52a8ebb9a5fe75fecee480f60582
erickd3v <moyae76@gmail.com>
2024-07-19 23:45:13 -0500

    refactor: update href format and add corresponding .astro files

--- src/components/reactComponents/Projects.tsx
+++ src/components/reactComponents/Projects.tsx
@@ -19,7 +19,7 @@ class Projects extends Component<Image> {
     return (
       <div>
         <p>{title}</p>
-        <a href={aHref}>
+        <a href={`/challenges/${aHref}-component`}>
           <img
             src={`cover-pages/tarea${imgSrc}`}
             alt={imgAlt}
@@ -34,110 +34,106 @@ class Projects extends Component<Image> {
 const projectData = [
   {
     title: "Sumary Components",
-    aHref: "results-summary-component-main",
+    aHref: "sumary",
     imgSrc: "1.webp",
     imgAlt: "Sumary Components",
   },
   {
     title: "Preview Card Component",
-    aHref: "product-preview-card-component-main",
+    aHref: "preview-card",
     imgSrc: "2.webp",
     imgAlt: "Preview Card Component",
   },
   {
     title: "Interactive Rating Component",
-    aHref: "interactive-rating-component-main",
+    aHref: "interactive-rating",
     imgSrc: "3.webp",
     imgAlt: "Interactive Rating Component",
   },
   {
     title: "Qr Code Component",
-    aHref: "qr-code-component-main",
+    aHref: "qr-code",
     imgSrc: "4.webp",
     imgAlt: "Qr Code Component",
   },
   {
     title: "NFT Card Component",
-    aHref: "nft-preview-card-component-main",
+    aHref: "nft-preview-card",
     imgSrc: "5.webp",
     imgAlt: "NFT Card Component",
   },
   {
     title: "Order Summary Component",
-    aHref: "order-summary-component-main",
+    aHref: "order-summary",
     imgSrc: "6.webp",
     imgAlt: "Order Summary Component",
   },
   {
     title: "Stats Preview Card",
-    aHref: "stats-preview-card-component-main",
+    aHref: "stats-preview-card",
     imgSrc: "7.webp",
     imgAlt: "Stats Preview Card",
   },
   {
     title: "3-Column Card Component",
-    aHref: "3-column-preview-card-component-main",
+    aHref: "3-column-preview-card",
     imgSrc: "8.webp",
     imgAlt: "3-Column Card Component",
   },
   {
     title: "Profile card component",
-    aHref: "profile-card-component-main",
+    aHref: "profile-card",
     imgSrc: "9.webp",
     imgAlt: "Profile card component",
   },
   {
     title: "Social proof section",
-    aHref: "social-proof-section-main",
+    aHref: "social-proof-section",
     imgSrc: "10.webp",
     imgAlt: "Social proof section",
   },
   {
     title: "Article previw component",
-    aHref: "article-preview-component-main",
+    aHref: "article-preview",
     imgSrc: "11.webp",
     imgAlt: "Article previw component",
   },
   {
     title: "Four card feature section",
-    aHref: "four-card-feature-section-main",
+    aHref: "four-card-feature-section",
     imgSrc: "12.webp",
     imgAlt: "Four card feature section",
   },
   // {
   //   title: "NFT Card Component",
-  //   aHref: "nft-preview-card-component-main",
+  //   aHref: "nft-preview-card",
   //   imgSrc: "13.webp",
   //   imgAlt: "NFT Card Component",
   // },
   // {
   //   title: "NFT Card Component",
-  //   aHref: "nft-preview-card-component-main",
+  //   aHref: "nft-preview-card",
   //   imgSrc: "14.webp",
   //   imgAlt: "NFT Card Component",
   // },
   // {
   //   title: "NFT Card Component",
-  //   aHref: "nft-preview-card-component-main",
+  //   aHref: "nft-preview-card",
   //   imgSrc: "15.webp",
   //   imgAlt: "NFT Card Component",
   // },
 ];
 
 const App = () => {
-  return (
-    <div>
-      {projectData.map((project, index) => (
-        <Projects
-          key={index}
-          title={project.title}
-          aHref={project.aHref}
-          imgSrc={project.imgSrc}
-          imgAlt={project.imgAlt}
-        />
-      ))}
-    </div>
-  );
+  return projectData.map((project, index) => (
+    <Projects
+      key={index}
+      title={project.title}
+      aHref={project.aHref}
+      imgSrc={project.imgSrc}
+      imgAlt={project.imgAlt}
+    />
+  ));
 };
 
 export default App;
